25cbdf4f004a47b459d2662d27b2f31ff6f08b40
kazuyuki-okada5 <razubelly0522@gmail.com>
2024-06-11 12:40:07 +0900

    appページ一部修正・メインページレイアウト完成

--- src/resources/views/items/item.blade.php
+++ src/resources/views/items/item.blade.php
@@ -5,73 +5,79 @@
 @endsection
 
 @section('content')
-
-<div class="listtype__content">
-  <div class="listtype__list">
-    <ul class="listtype-nav">
-      <li class="listtype-nav__item">
-        <button class="listtype-nav__button" id="show-items">おすすめ</button>
-      </li>
-
-      @if (Auth::check())
-      <li class="listtype-nav__item">
-        <button class="listtype-nav__button" id="show-likes">マイリスト</button>
-      @endif
-    </ul>
-  </div>
-</div>
-
-<!--アイテム一覧-->
-<div id="item-list" class="row mt-4">
-  <h3>アイテム一覧</h3>
-  @foreach($items as $item)
-  <div class="col-md-4">
-    <div class="card mb-4">
-      <a href="{{ route('items.show', $item->id )}}">
-        <img src="{{ asset('storage/' . $item->image_url) }}" class="card-img-top" alt="{{ $item->name }}">
-      </a>
-      <div class="card-body">
-        <h5 class="card-title">{{ $item->name }}</h5>
-        <p class="card-text">{{ $item->price }}円</p>
-      </div>
+<div class="central-container">
+    <div class="listtype__content">
+        <div class="listtype__list">
+            <ul class="listtype-nav">
+                <li class="listtype-nav__item">
+                    <button class="listtype-nav__button selected" id="show-items">おすすめ</button>
+                </li>
+                @if (Auth::check())
+                <li class="listtype-nav__item">
+                    <button class="listtype-nav__button" id="show-likes">マイリスト</button>
+                </li>
+                @endif
+            </ul>
+        </div>
     </div>
-  </div>
-  @endforeach
-</div>
 
-@if (Auth::check())
-<!-- お気に入り一覧 -->
-<div id="likes-list" class="row mt-4" style="display: none;">
-  <h3>マイリスト一覧</h3>
-  @foreach ($likes as $like)
-  <div class="col-md-4">
-    <div class="card mb-4">
-      <a href="{{ route('items.show', $like->item->id )}}">
-        <img src="{{ asset('storage/' . $like->item->image_url) }}" class="card-img-top" alt="{{ $like->item->name }}">
-      </a>
-      <div class="card-body">
-        <h5 class="card-title">{{ $like->item->name }}</h5>
-        <p class="card-text">{{ $like->item->price }}円</p>
-      </div>
+    <!--アイテム一覧-->
+    <div id="item-list" class="item-container">
+        @foreach($items as $item)
+        <div class="item card-img-top">
+            <div class="item">
+                <a href="{{ route('items.show', $item->id )}}">
+                    <img src="{{ asset('storage/' . $item->image_url) }}" class="card-img-top" alt="{{ $item->name }}">
+                </a>
+                <p class="card-text"><span>{{ $item->price }}円</span></p>
+                <div class="card-body">
+                    <h2 class="card-title">{{ $item->name }}</h2>
+                </div>
+            </div>
+        </div>
+        @endforeach
     </div>
-  </div>
-  @endforeach
+
+    @if (Auth::check())
+    <!-- お気に入り一覧 -->
+    <div id="likes-list" class="item-container hidden">
+        @foreach ($likes as $like)
+        <div class="item card-img-top">
+            <div class="item">
+                <a href="{{ route('items.show', $like->item->id )}}">
+                    <img src="{{ asset('storage/' . $like->item->image_url) }}" class="card-img-top" alt="{{ $like->item->name }}">
+                </a>
+                <p class="card-text"><span>{{ $like->item->price }}円</span></p>
+                <div class="card-body">
+                    <h2 class="card-title">{{ $like->item->name }}</h2>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+    @endif
 </div>
-@endif
 
 <script>
-  document.getElementById('show-items').addEventListener('click', function(){
-    document.getElementById('item-list').style.display = 'block';
-    if (document.getElementById('likes-list')) {
-        document.getElementById('likes-list').style.display = 'none';
-    }
-  });
-
-  if (document.getElementById('show-likes')) {
-    document.getElementById('show-likes').addEventListener('click', function(){
-      document.getElementById('item-list').style.display = 'none';
-      document.getElementById('likes-list').style.display = 'block';
+    document.getElementById('show-items').addEventListener('click', function(){
+        document.getElementById('item-list').style.display = 'block';
+        if (document.getElementById('likes-list')) {
+            document.getElementById('likes-list').style.display = 'none';
+        }
+        document.getElementById('show-items').classList.add('selected');
+        document.getElementById('show-likes').classList.remove('selected');
     });
-  }
+
+    document.getElementById('show-likes').addEventListener('click', function(){
+        document.getElementById('item-list').style.display = 'none';
+        if (document.getElementById('likes-list')) {
+            document.getElementById('likes-list').style.display = 'block';
+        }
+        document.getElementById('show-items').classList.remove('selected');
+        document.getElementById('show-likes').classList.add('selected');
+    });
 </script>
 @endsection
+
+
+
--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="ja">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,54 +11,54 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     @yield('css')
 </head>
-
 <body>
-    <header class="header">
-        <div class="header__inner">
-            <div class="header__logo">
-                <img src="{{ asset('storage/logo (1).svg') }}" alt="Flea Market App Logo" class="logo">
+    <div class="page-wrapper">
+        <header class="header">
+            <div class="header__inner">
+                <div class="header__logo">
+                    <img src="{{ asset('storage/logo (1).svg') }}" alt="Flea Market App Logo" class="logo">
+                </div>
+                <div class="header__search">
+                    <form class="form" action="{{ route('items.search') }}" method="get">
+                        <input type="text" name="query" class="search-input" placeholder="なにをお探しですか？">
+                    </form>
+                </div>
+                <nav class="header-nav">
+                    <ul class="header-nav__list">
+                        @if (Auth::check())
+                        <li class="header-nav__item">
+                            <a class="header-nav__link" href="{{ route('items.create') }}">出品</a>
+                        </li>
+                        <li class="header-nav__item">
+                            <a class="header-nav__link" href="/">トップページ</a>
+                        </li>
+                        <li class="header-nav__item">
+                            <a class="header-nav__link" href="{{ route('mypage') }}">マイページ</a>
+                        </li>
+                        <li class="header-nav__item">
+                            <form class="form" action='/logout' method="post">
+                                @csrf
+                                <button class="header-nav__button">ログアウト</button>
+                            </form>
+                        </li>
+                        @else
+                        <li class="header-nav__item">
+                            <a class="header-nav__link" href="/">トップページ</a>
+                        </li>
+                        <li class="header-nav__item">
+                            <a class="header-nav__link" href="/login">ログイン</a>
+                        </li>
+                        <li class="header-nav__item">
+                            <a class="header-nav__link" href="/register">会員登録</a>
+                        </li>
+                        @endif
+                    </ul>
+                </nav>
             </div>
-            <div class="header__search">
-                <form class="form" action="{{ route('items.search') }}" method="get">
-                    <input type="text" name="query" class="search-input" placeholder="なにをお探しですか？">
-                </form>
-            </div>
-            <nav class="header-nav">
-                <ul class="header-nav__list">
-                    @if (Auth::check())
-                    <li class="header-nav__item">
-                        <a class="header-nav__link" href="{{ route('items.create') }}">出品</a>
-                    </li>
-                    <li class="header-nav__item">
-                        <a class="header-nav__link" href="/">トップページ</a>
-                    </li>
-                    <li class="header-nav__item">
-                        <a class="header-nav__link" href="{{ route('mypage') }}">マイページ</a>
-                    </li>
-                    <li class="header-nav__item">
-                        <form class="form" action='/logout' method="post">
-                            @csrf
-                            <button class="header-nav__button">ログアウト</button>
-                        </form>
-                    </li>
-                    @else
-                    <li class="header-nav__item">
-                        <a class="header-nav__link" href="/">トップページ</a>
-                    </li>
-                    <li class="header-nav__item">
-                        <a class="header-nav__link" href="/login">ログイン</a>
-                    </li>
-                    <li class="header-nav__item">
-                        <a class="header-nav__link" href="/register">会員登録</a>
-                    </li>
-                    @endif
-                </ul>
-            </nav>
-        </div>
-    </header>
-    <main>
-        @yield('content')
-    </main>
+        </header>
+        <main>
+            @yield('content')
+        </main>
+    </div>
 </body>
-
 </html>
