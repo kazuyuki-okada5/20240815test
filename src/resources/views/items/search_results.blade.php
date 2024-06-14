f3e1c739473be1fe6dc072357850ee5db05da80a
kazuyuki-okada5 <razubelly0522@gmail.com>
2024-06-14 17:25:36 +0900

    検索結果画面レイアウト作成

--- src/resources/views/items/search_results.blade.php
+++ src/resources/views/items/search_results.blade.php
@@ -1,22 +1,30 @@
 @extends('layouts.app')
 
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/items/search.css') }}">
+@endsection
+
 @section('content')
 <div class="container">
     <h1>検索結果</h1>
     @if($items->isEmpty())
         <p>該当するアイテムがありません</p>
     @else
-        <div class="row">
+        <div class="list-conteneres">
             @foreach ($items as $item)
-                <div class="col-md-4">
-                    <div class="card mb-4">
+                <div class="conteneres">
+                    
+                    <div class="list">
+                        <a href="{{ route('items.show', $item->id) }}">
                         <img src="{{ asset('storage/' . $item->image_url) }}" class="card-img-top" alt="{{ $item->name }}">
+                        </a>
+                        <p class="card-text"><span>{{ $item->price }}円</span></p>
                         <div class="card-body">
-                            <h5 class="card-title">{{ $item->name }}</h5>
-                            <p class="card-text">{{ $item->price }}円</p>
-                            <a href="{{ route('items.show', $item->id) }}" class="btn btn-primary">詳細を見る</a>
+                            <p class="card-title">{{ $item->name }}</p>
+
                         </div>
                     </div>
+                    
                 </div>
             @endforeach
         </div>
