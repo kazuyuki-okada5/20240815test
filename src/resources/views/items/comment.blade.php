f5c3925e5e14bf15a0e1243971e465eeb474beb6
kazuyuki-okada5 <razubelly0522@gmail.com>
2024-06-25 09:30:17 +0900

    コメントページ売り切れ後の表示内容変更レイアウト一部修正

--- src/resources/views/items/comment.blade.php
+++ src/resources/views/items/comment.blade.php
@@ -7,10 +7,15 @@
 @section('content')
 <div class="item-detail">
     <div class="item-info">
-        <img src="{{ asset('storage/' . $item->image_url) }}" alt="{{ $item->name }}" style="max-width: 300px;">
+        <div class="item-image-container">
+            <img src="{{ asset('storage/' . $item->image_url) }}" alt="{{ $item->name }}" class="item-image">
+            @if($item->sold_user_id)
+                <div class="sold-out-overlay">SOLD-OUT</div>
+            @endif
+        </div>
         <p class="name">{{ $item->name }}</p>
         <p class="brand">{{ $item->brand }}</p>
-        <p class="price">価格:￥{{ $item->price }}（税込）</p>
+        <p class="price">{{ $item->sold_user_id ? '売り切れ' : '価格:￥' .$item->price . '(税込)' }}</p>
         <div class="like-container">
             <form id="like-form" method="POST" action="{{ route('likes.like', $item->id )}}" style="display: {{ $isLiked ? 'none' : 'inline' }}">
                 @csrf
@@ -36,75 +41,62 @@
     </div>
 
     <div class="item-comments">
-        <h2 class="commnent-list">コメント一覧</h2>
-@foreach($comments as $comment)
-    <div class="comment">
-        @if($comment->userProfile)
-            @if($comment->user_id === $item->user_id)
-                <div class="comment-right">
-                    @if($comment->userProfile->img_url)
-                        <img src="{{ asset('storage/' . $comment->userProfile->img_url) }}" class="comment-avatar" alt="{{ $comment->userProfile->name }}">
-                    @else
-                        <div class="default-avatar">
-                            <i class="fas fa-user-circle"></i><!-- 代替えアイコン -->
-                        </div>
-                    @endif
-                        <p class="comment"><strong>{{ $comment->userProfile->name }}</strong></p>
+        <h2 class="comment-list">コメント一覧</h2>
+        @foreach($comments as $comment)
+            <div class="comment">
+                @if($comment->user_id === $item->user_id)
+                    <div class="comment-right">
+                        @if($comment->userProfile && $comment->userProfile->img_url)
+                            <img src="{{ asset('storage/' . $comment->userProfile->img_url) }}" class="comment-avatar-right" alt="{{ $comment->userProfile->name }}">
+                        @else
+                            <div class="default-avatar">
+                                <i class="fas fa-user-circle"></i><!-- 代替えアイコン -->
+                            </div>
+                        @endif
                         <p class="date-right">（{{ $comment->created_at->format('Y-m-d H:i') }}）</p>
-                </div>
-                <div class="comment-text-right">
-                    <p class="comment">{{ $comment->comment }}</p>
-                </div>
-            @else
-                <div class="comment-left">
-                    @if($comment->userProfile->img_url)
-                        <img src="{{ asset('storage/' . $comment->userProfile->img_url) }}" class="comment-avatar" alt="{{ $comment->userProfile->name }}">
-                    @else
-                        <div class="default-avatar">
-                            <i class="fas fa-user-circle"></i><!-- 代替えアイコン -->
-                        </div>
-                    @endif
-                        <p class="comment"><strong>{{ $comment->userProfile->name }}</strong></p>
+                    </div>
+                    <div class="comment-text-right">
+                        <p class="comment">{{ $comment->comment }}</p>
+                    </div>
+                @else
+                    <div class="comment-left">
+                        @if($comment->userProfile && $comment->userProfile->img_url)
+                            <img src="{{ asset('storage/' . $comment->userProfile->img_url) }}" class="comment-avatar-left" alt="{{ $comment->userProfile->name }}">
+                        @else
+                            <div class="default-avatar">
+                                <i class="fas fa-user-circle"></i><!-- 代替えアイコン -->
+                            </div>
+                        @endif
                         <p class="date-left">（{{ $comment->created_at->format('Y-m-d H:i') }}）</p>
-                </div>
-                <div class="comment-text-left">
-                    <p class="comment">{{ $comment->comment }}</p>
-                </div>
-            @endif
-        @else
-            <!-- ユーザープロファイルが存在しない場合の処理 -->
-            <div class="comment-left">
-                <div class="default-avatar">
-                    <i class="fas fa-user-circle"></i><!-- 代替えアイコン -->
-                </div>
-                <div>
-                    <p class="date-left">（{{ $comment->created_at->format('Y-m-d H:i') }}）</p>
-                </div>
-            </div>
-            <div class="comment-text-left">
-                <p class="comment">{{ $comment->comment }}</p>
-            </div>
-        @endif
-    </div>
-@endforeach
-
-@auth
-    <form method="POST" action="{{ route('comments.store', ['item' => $item->id]) }}" class="comment-form">
-        @csrf
-        <div class="form-group">
-            <label for="comment">商品へのコメント</label>
-            <textarea name="comment" id="comment" class="form-control" rows="3" required>{{ old('comment') }}</textarea>
-                @if ($errors->has('comment'))
-                    <div class="alert alert-danger">
-                        <span class="error-message">{{ $errors->first('comment') }}</span>
+                    </div>
+                    <div class="comment-text-left">
+                        <p class="comment">{{ $comment->comment }}</p>
                     </div>
                 @endif
             </div>
-            <button type="submit" class="btn btn-primary">コメントを投稿</button>
-        </form>
-    @else
-       <p class="login-required-message">コメントを投稿するには<a href="{{ route('login') }}" class="login-link">ログイン</a>が必要です</p>
-    @endauth
+        @endforeach
+
+        @auth
+            @if(is_null($item->sold_user_id))
+            <form method="POST" action="{{ route('comments.store', ['item' => $item->id]) }}" class="comment-form">
+                @csrf
+                <div class="form-group">
+                    <label for="comment">商品へのコメント</label>
+                    <textarea name="comment" id="comment" class="form-control" rows="3" required>{{ old('comment') }}</textarea>
+                    @if ($errors->has('comment'))
+                        <div class="alert alert-danger">
+                            <span class="error-message">{{ $errors->first('comment') }}</span>
+                        </div>
+                    @endif
+                </div>
+                <button type="submit" class="btn btn-primary">コメントを投稿</button>
+            </form>
+            @else
+                <p class="sold-out-required-message">購入後の品にコメントは出来ません。</p>
+            @endif
+        @else
+            <p class="login-required-message">コメントを投稿するには<a href="{{ route('login') }}" class="login-link">ログイン</a>が必要です</p>
+        @endauth
     </div>
 </div>
 @endsection
@@ -149,3 +141,4 @@
     });
 </script>
 @endsection
+
