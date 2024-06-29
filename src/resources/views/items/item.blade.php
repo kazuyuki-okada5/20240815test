0c69545bdfe43d1933f368390f398fb6f3a44dc4
kazuyuki-okada5 <razubelly0522@gmail.com>
2024-06-29 18:57:09 +0900

    アイテム一覧ページに販売中リストを追加

--- src/resources/views/items/item.blade.php
+++ src/resources/views/items/item.blade.php
@@ -12,16 +12,19 @@
                 <li class="listtype-nav__item">
                     <button class="listtype-nav__button selected" id="show-items">おすすめ</button>
                 </li>
+                <li class="listtype-nav__item">
+                    <button class="listtype-nav__button" id="show-available">販売中</button>
+                </li>
                 @if (Auth::check())
                 <li class="listtype-nav__item">
                     <button class="listtype-nav__button" id="show-likes">マイリスト</button>
-                @endif
                 </li>
+                @endif
             </ul>
         </div>
     </div>
 
-    <!--アイテム一覧-->
+    <!-- おすすめアイテム一覧 -->
     <div id="item-list" class="item-container">
         @foreach($items as $item)
         <div class="item">
@@ -66,6 +69,23 @@
         @endforeach
     </div>
     @endif
+
+    <!-- 販売中リスト -->
+    <div id="available-list" class="item-container hidden">
+        @foreach($items as $item)
+        @if (!$item->sold_user_id)
+        <div class="item">
+            <a href="{{ route('items.show', $item->id )}}">
+                <img src="{{ asset('storage/' . $item->image_url) }}" class="card-img-top" alt="{{ $item->name }}">
+            </a>
+            <p class="card-text">
+                <span>{{ $item->price }}円</span>
+            </p>
+            <p class="card-title">{{ $item->name }}</p>
+        </div>
+        @endif
+        @endforeach
+    </div>
 </div>
 
 <script>
@@ -74,20 +94,44 @@
         if (document.getElementById('likes-list')) {
             document.getElementById('likes-list').classList.add('hidden');
         }
+        document.getElementById('available-list').classList.add('hidden');
+
         document.getElementById('show-items').classList.add('selected');
-        document.getElementById('show-likes').classList.remove('selected');
+        if (document.getElementById('show-likes')) {
+            document.getElementById('show-likes').classList.remove('selected');
+        }
+        document.getElementById('show-available').classList.remove('selected');
     });
 
-    document.getElementById('show-likes').addEventListener('click', function(){
+    if (document.getElementById('show-likes')) {
+        document.getElementById('show-likes').addEventListener('click', function(){
+            document.getElementById('item-list').classList.add('hidden');
+            document.getElementById('likes-list').classList.remove('hidden');
+            document.getElementById('available-list').classList.add('hidden');
+
+            document.getElementById('show-items').classList.remove('selected');
+            document.getElementById('show-likes').classList.add('selected');
+            document.getElementById('show-available').classList.remove('selected');
+        });
+    }
+
+    document.getElementById('show-available').addEventListener('click', function(){
         document.getElementById('item-list').classList.add('hidden');
         if (document.getElementById('likes-list')) {
-            document.getElementById('likes-list').classList.remove('hidden');
+            document.getElementById('likes-list').classList.add('hidden');
         }
+        document.getElementById('available-list').classList.remove('hidden');
+
         document.getElementById('show-items').classList.remove('selected');
-        document.getElementById('show-likes').classList.add('selected');
+        if (document.getElementById('show-likes')) {
+            document.getElementById('show-likes').classList.remove('selected');
+        }
+        document.getElementById('show-available').classList.add('selected');
     });
 </script>
 @endsection
+
+
 
 
 
