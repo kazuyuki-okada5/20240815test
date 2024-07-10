417e75014f5bd9eaa1adb69b8cfde2fa16e05262
kazuyuki-okada5 <razubelly0522@gmail.com>
2024-07-10 10:23:48 +0900

    ユーザー管理画面一部修正

--- src/resources/views/admin/users.blade.php
+++ src/resources/views/admin/users.blade.php
@@ -16,17 +16,17 @@
     <div class="mail-form">
         <form action="{{ route('admin.users.sendEmail') }}" method="POST">
         @csrf
-        <div class="form-group">
-            <label for="subject">件名</label>
-            <input type="text" name="subject" class="form-control" required>
-        </div>
-        <div class="form-group">
-            <label for="message">本文</label>
-            <textarea name="message" class="form-control" rows="5" required></textarea>
-        </div>
-        <div class="form-group button-right">
-            <button type="submit" class="btn btn-primary">全員にメールを送信</button>
-        </div>
+            <div class="form-group">
+                <label class="form-group-label" for="subject">件名</label>
+                <input type="text" name="subject" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label class="form-group-label" for="message">本文</label>
+                <textarea name="message" class="form-control" rows="5" required></textarea>
+            </div>
+            <div class="form-group button-right">
+                <button type="submit" class="btn btn-primary">全員にメールを送信</button>
+            </div>
         </form>
     </div>
     <table class="table table-striped">
@@ -48,11 +48,11 @@
                     <td class="table-cell-role">{{ $user->role == 0 ? '管理者' : 'ユーザー' }}</td>
                     <td class="table-cell-button">
                         @if($user->role != 0) <!-- 管理者は削除出来ない-->
-                        <form class="delete-form" action="{{ route('admin.users.delete', $user->id) }}" method="POST" onsubmit="return confirm('本当に削除しますか？');">
+                            <form class="delete-form" action="{{ route('admin.users.delete', $user->id) }}" method="POST" onsubmit="return confirm('本当に削除しますか？');">
                             @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">削除</button>
-                        </form>
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">削除</button>
+                            </form>
                         @endif
                     </td>
                 </tr>
